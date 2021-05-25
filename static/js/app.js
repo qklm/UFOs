dbf0b89611574aed500243d98381e8a7f2dd8ca8
David Brown <quinsoq@gmail.com>
2021-05-25 06:14:09 -0700

    initial commit

---
+++ static/js/app.js
@@ -0,0 +1,14 @@
+// import the data from data.js
+const tableData = data;
+var tbody = d3.select("tbody");
+function buildTable(data) {
+    tbody.html("");
+    data.forEach((dataRow) => {
+        let row = tbody.append("tr");
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+            }
+        );
+    });
+}
